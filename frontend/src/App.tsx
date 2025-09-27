360da486a0160546182c35e1226b18f8f99b3c70
Amen Khnissi <amenkhnissi@gmal.com>
2025-09-27 15:53:03 +0200

    Add <SpeedInsights />

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/ui/navBar'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <div className="felx flex-col justify-center  h-lvh w-full bg-white backdrop-blur-md">
       <NavBar />
       <Outlet />
+      <SpeedInsights />
     </div>
 
   )
